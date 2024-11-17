73aad30be314a996d1638e8788c186a21f644ffe
Nicholas Yeung <njyeung@wisc.edu>
2024-11-16 22:59:54 -0600

    n

--- src/components/content/Editor.jsx
+++ src/components/content/Editor.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext} from "react"
-import {Col, Row, Container, Button, Card} from "react-bootstrap"
+import {Col, Row, Container, Button, Card, Form} from "react-bootstrap"
 
 import Annotation from './Annotation'
 
@@ -7,6 +7,35 @@ export default function Editor(props) {
 
     const [active, setActive] = useState("Quick Create");
 
+    function panel() {
+        switch(active){
+            case 'Quick Create': return <>
+                <Form.Label htmlFor="inputPassword5">Label</Form.Label>
+                <Form.Control id="inputPassword5"/>
+                <Form.Text id="passwordHelpBlock" muted>
+                    Your password must be 8-20 characters long, contain letters and numbers,
+                    and must not contain spaces, special characters, or emoji.
+                </Form.Text>
+                <Form.Control as="textarea" rows={3} />
+            </>
+            case 'Position': return <>
+            </>
+            case 'Text': return <>
+                <label>Title</label>
+                <input type="text" />
+                <label>Body</label>
+                <input type="text" />
+                <label>Float / Surface</label>
+                <Form.Check type="switch" />
+                <textarea></textarea>
+            </>
+            case 'Line': return <>
+            </>
+            case 'Spot': return <>
+            </>
+            default: return <></>
+        }
+    }
     return <div>
         <Container>
             <Row>
@@ -31,10 +60,8 @@ export default function Editor(props) {
         <br />
         <br />
         <Container>
-            <Card>
-                <div>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus rem dolorem molestias quia nesciunt fugiat reprehenderit commodi eveniet corrupti deserunt!</p>
-                </div>
+            <Card style={{padding: '2rem'}}>
+                {panel()}
             </Card>
         </Container>
     </div>

e93fdc3f77f0b0d21c0f3c49de4cd0bb33d0d8c9
Nicholas Yeung <njyeung@wisc.edu>
2024-11-20 20:41:28 -0600

    I DUNNO HOW THIS WORKS

--- src/components/content/Annotation.jsx
+++ src/components/content/Annotation.jsx
@@ -3,22 +3,6 @@ import { useEffect, useState } from "react";
 import { Card, Button, Carousel, Overlay } from "react-bootstrap";
 
 function Annotation(props) {
-
-    const [prod, setProd] = useState(true);
-
-    // check if we're in dev or prod for the image url LMAO
-    useEffect(()=>{
-        checkIfImageExists(`./${images[0]}`, (exists)=> {
-            if(exists) {
-                console.log('IN DEV MODE')
-                setProd(false);
-            }
-            else {
-                console.log('IN PROD')
-            }
-        })
-    },[]);
-
     const active = props.active
 
     const header = `images/${props.title.replace(' ', '').toLowerCase()}`
@@ -36,7 +20,7 @@ function Annotation(props) {
         <div style={{width: '100%', position: 'relative', textAlign: 'center'}}>
             <Carousel interval={2000} pause={false} controls={false} variant="dark">
                 {images.map((img)=> <Carousel.Item key={img}>
-                    <img style={active==props.title ? {...styles.imageActive} : {...styles.image}} src={prod ? `./cs2-nade-guide/${img}` : `./${img}`} alt={`Image for ${props.title}`} />
+                    <img style={active==props.title ? {...styles.imageActive} : {...styles.image}} src={`./${img}`} alt={`Image for ${props.title}`} />
                 </Carousel.Item>)}
             </Carousel>
             <h1 style={{ position: 'absolute', color: "white", top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>{props.title}</h1>
